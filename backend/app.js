1e0f90c6e1d3eafbfb3a09ebe8d7002dff2be99d
Satyabrat Divedi <raghavstar1991@gmail.com>
2024-10-15 08:46:10 +0530

    Set specific origin for CORS headers

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ app.use(cookieParser());
 app.use(
   cors({
     // origin: "https://form-bot.skdivedi.me",
-    origin: "*",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
